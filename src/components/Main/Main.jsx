074094fba391404fdfc2e927636c9dcbb81e8e83
XimenaRodriguez <ximerm138@gmail.com>
2026-07-13 12:11:50 -0500

    Create Main.jsx structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,23 @@
 import "./Main.css";
 
+import SearchForm from "../SearchForm/SearchForm";
+import About from "../About/About";
+
 function Main() {
-  return <main>Main</main>;
+  return (
+    <main className="main">
+      <section className="hero">
+        <h1>EcoBuildLab</h1>
+        <p>
+          Explora estrategias de diseño sostenible mediante datos climáticos.
+        </p>
+      </section>
+
+      <SearchForm />
+
+      <About />
+    </main>
+  );
 }
 
 export default Main;
---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,11 @@
+import "./SearchForm.css";
+
+function SearchForm() {
+  return (
+    <section className="search-form">
+      <h2>Search Form</h2>
+    </section>
+  );
+}
+
+export default SearchForm;
